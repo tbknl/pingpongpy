4950f7a857ba905a10d38c0170ea27b4a456cf48
Dave van Soest <dave.soest@gfk.com>
2015-05-28 14:10:09 +0200

    First implementation version. Documentation in readme.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# pingpongpy
-PingPongPy - Simple TCP network latency test tool
+# PingPongPy
+Simple TCP network latency test tool.
+
+First start a server on one host, then start a client on another host which will measure the latency.  A ping packet is sent to the server and the server will respond with a pong packet. The next ping packet is sent only after the full pong packet is received. The roundtrip time is measured for each combination of ping request and pong response. Statistics (average, min, max, std.dev.) are printed for the roundtrip times when ready.
+
+
+## Usage
+
+### Server
+Start a server:
+```
+# ./pingpong.py pong ALL 1234
+```
+This will bind the socket to all addresses and start listening on port 1234.
+
+The server will serve one client at a time, while letting the others wait.
+
+
+### Client
+Start a client:
+```
+# ./pingpong.py ping server_hostname.example.com 1234 --ping-size 50000 --pong-size 1000 --seconds 10
+```
+This will start a client that connects to the specified server and port. The size of the ping payload is 50000 bytes and the size of the pong payload is 1000 bytes. It will stop sending ping requests after 10 seconds and will terminate after the pong response to the final ping request is fully received.
+
+
+## Example output
+```
+# ./pingpong.py ping localhost 1234 --seconds 5 --ping-size 500000 --pong-size 100000
+seq     roundtrip-time  elapsed-time
+1       0.455527067184  0.45556306839
+2       0.391165018082  0.846796035767
+3       0.408379793167  1.25522899628
+4       0.473116159439  1.7283911705
+5       0.481930971146  2.21041607857
+6       0.517910003662  2.72848510742
+7       0.449773788452  3.17840099335
+8       0.513982772827  3.69247198105
+9       0.458905220032  4.1514852047
+10      0.448948860168  4.60057902336
+11      0.494635105133  5.09529209137
+Connection closed.
+Roundtrips: 11
+Average: 0.463115887208
+Min: 0.391165018082
+Max: 0.517910003662
+Stddev: 0.0377363014256
+```
---
+++ pingpong.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import argparse
+import socket
+import sys
+import string
+import random
+import time
+import numpy
+
+RANDOM_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+
+def pong_server(host, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	try:
+		sock.bind((host, port))
+	except socket.error as err:
+		print("Socket bind failed.", err)
+		sys.exit(1)
+
+	sock.listen(0)
+	print("Listening on {0}:{1}...".format(host, port))
+	try:
+		while True:
+			conn, addr = sock.accept()
+			print("Incoming connection from {0}:{1}".format(*addr))
+			file_wrapper = conn.makefile("r")
+			try:
+				while True:
+					line = file_wrapper.readline()
+					if not line:
+						break
+					cmd = line.split()
+					if len(cmd) >= 2 and cmd[0] == "ping":
+						pong_size = 0
+						try:
+							pong_size = max(0, int(cmd[1]))
+						except ValueError:
+							pass
+						conn.send("pong ")
+						while pong_size > 0:
+							chunk_size = min(pong_size, 4096)
+							data = string.join([random.choice(RANDOM_CHARS) for x in range(chunk_size)], '')
+							conn.send(data)
+							pong_size -= chunk_size
+						conn.send("\n")
+					else:
+						print("Invalid command received.")
+						break
+			except socket.error:
+				pass
+			finally:
+				file_wrapper.close()
+				conn.close()
+				print("Connection closed.")
+	except KeyboardInterrupt:
+		print("Interrupted. Shutting down.")
+
+	sock.close()
+
+
+def ping(host, port, ping_size = 10, pong_size = 500000, seconds = 10):
+	roundtrips = []
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		sock.connect((host, port))
+		file_wrapper = sock.makefile("r")
+		start_time = time.time()
+		try:
+			r = 1
+			now = 0
+			print("seq\troundtrip-time\telapsed-time")
+			while now < start_time + seconds:
+				t0 = time.time()
+				sock.send("ping {0} ".format(pong_size))
+				data_left = ping_size
+				while data_left > 0:
+					chunk_size = min(data_left, 4096)
+					data = string.join([random.choice(RANDOM_CHARS) for x in range(chunk_size)], '')
+					sock.send(data)
+					data_left -= chunk_size
+				sock.send("\n")
+				line = file_wrapper.readline()
+				now = time.time()
+				dt = now - t0
+				roundtrips.append(dt)
+				print("{0}\t{1}\t{2}".format(r, dt, now - start_time))
+				r += 1
+		finally:
+			file_wrapper.close()
+	except KeyboardInterrupt:
+		print("Interrupted. Shutting down.")
+	except:
+		print("Unexpected exception: {0}".format(sys.exc_info()))
+	finally:
+		sock.close()
+		print("Connection closed.")
+
+	print("Roundtrips: {count}".format(count=len(roundtrips)))
+	print("Average: {average}".format(average=numpy.mean(roundtrips)))
+	print("Min: {min}".format(min=numpy.min(roundtrips)))
+	print("Max: {max}".format(max=numpy.max(roundtrips)))
+	print("Stddev: {stddev}".format(stddev=numpy.std(roundtrips)))
+
+
+if __name__ == "__main__":
+	argparser = argparse.ArgumentParser()
+	argparser.add_argument("mode", type=str, choices=['ping', 'pong'], help="program mode")
+	argparser.add_argument("host", type=str, help="host ip address (in pong mode 'ALL' indicates bind to all addresses)")
+	argparser.add_argument("port", type=int, help="tcp port")
+	argparser.add_argument("--ping-size", type=int, default=10000, help="ping payload size (only in ping mode)")
+	argparser.add_argument("--pong-size", type=int, default=500, help="pong payload size (only in ping mode)")
+	argparser.add_argument("--seconds", type=int, default=30, help="duration in seconds (only in ping mode)")
+	args = argparser.parse_args()
+
+	if args.mode == 'pong':
+		host = '0.0.0.0' if args.host == 'ALL' else args.host
+		pong_server(host, args.port)
+	else:
+		try:
+			host = socket.gethostbyname(args.host)
+		except:
+			print("Could not resolve host '{0}'.".format(args.host))
+			exit(1)
+
+		ping(host, args.port, ping_size=args.ping_size, pong_size=args.pong_size, seconds=args.seconds)
+
